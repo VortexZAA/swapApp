718ef7cafa37c9e65d592070e1138a259242f1b3
Tuğgün Harun Asrak <63567525+Morte-Bianca@users.noreply.github.com>
2023-10-08 18:56:56 +0300

    mint added for register

--- BaseEther.ts
+++ BaseEther.ts
@@ -1,12 +1,17 @@
 import * as dotenv from "dotenv";
-import { callNFTContract } from "./ethereumContracts";
+import { callNFTContract, callTokenContract } from "./ethereumContracts";
 
 dotenv.config();
 
 const priceTag = process.env.PRICE_TAG || "";
 const paymentToken = process.env.BASE_PAYMENT_TOKEN_CONTRACTADDRESS || "";
 
+export const callMint = async (holderAddress: string, amount: number) => {
+    (await callTokenContract()).contractWithSigner.mint(holderAddress, amount);
+};
+
 export const callRegister = async (domainName: string, holderAddress: string, tokenURI:string) => {
     const domain = [domainName, holderAddress, tokenURI, priceTag, paymentToken];
     (await callNFTContract()).contractWithSigner.register(domain);
 };
+
--- ethereumContracts.ts
+++ ethereumContracts.ts
@@ -3,6 +3,7 @@ declare global {
     interface Window { ethereum: any; }
   }
 const nftAddress = process.env.BASE_MOSEIKI_DOMAIN_NFT_CONTRACTADDRESS as string;
+const tokenAddress = process.env.BASE_PAYMENT_TOKEN_CONTRACTADDRESS as string;
 
 export const callNFTContract = async () => {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -12,4 +13,14 @@ export const callNFTContract = async () => {
     const myNFTContract = new ethers.Contract(nftAddress, abi, signer);
     const contractWithSigner = myNFTContract.connect(signer);
     return { contractWithSigner, nftAddress, abi }
+}
+
+export const callTokenContract = async () => {
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const abi = require("./abi.json");    
+  const myNFTContract = new ethers.Contract(tokenAddress, abi, signer);
+  const contractWithSigner = myNFTContract.connect(signer);
+  return { contractWithSigner, tokenAddress, abi }
 }
