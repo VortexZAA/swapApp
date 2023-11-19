38284f2bfb50fccf6cc0cd3d00f14ba2ff10bce5
Zeynel Abidin AYHAN <88234880+VortexZAA@users.noreply.github.com>
2023-11-19 03:25:12 +0300

    Revert "test"

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body
-        style={{
-          background: "linear-gradient(124deg, #161620 0%, #272733 100%)",
-        }}
-      >
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
